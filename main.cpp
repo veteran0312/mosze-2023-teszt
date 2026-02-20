c3c5f8a2540173f9c846aadd7a876ae36c75f864
Adrián <rigo.imre.adrian007@gmail.com>
2026-02-20 14:57:10 +0100

    Programhibák kijavítva

--- main.cpp
+++ main.cpp
@@ -4,41 +4,42 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // HIBA: N_ELEMENTS helyett NELEMENTS lett írva.
-    int b = new int[NELEMENTS];
+    
+    int b = new int[N_ELEMENTS];
 
-    // HIBA: Sztringekhez dupla idézőjel (""), és hiányzik a pontosvessző a sor végéről.
-    std::cout << '1-100 ertekek duplazasa'
+    
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
 
-    // HIBA: A for ciklus hiányos, hiányzik a léptetéts és a leállási feltétel.
-    for (int i = 0;)
+    
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i 2;
     }
 
-    // HIBA: A leállási feltétel hibás (i), így a ciklus egyszer sem fut le.
-    for (int i = 0; i; i++)
+    
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // HIBA: Nem ír ki értéket, és hiányzik a pontosvessző a végéről.
-        std::cout << "Ertek:"
+        
+        std::cout << "Ertek: " << b[i] << std::endl;
     }
 
     std::cout << "Atlag szamitasa: " << std::endl;
 
-    // HIBA: Az atlag változónak nincs megadva kezdőérték.
-    int atlag;
+    
+    int atlag = 0;
 
-    // HIBA: A feltétel és a léptetés között pontosvesszőnek kellene lennie (N_ELEMNTS után csak sima vessző van).
-    for (int i = 0; i < N_ELEMENTS, i++)
+    
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // HIBA: Pontosvessző hiányzik a sor végéről.
-        atlag += b[i]
+        
+        atlag += b[i];
     }
 
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
 
     
-    //HIBA: dinamikus memóriafoglalás felszabadítása elmaradt (delete [] b;)
+    delete[] b;
+
     return 0;
 }
